4ae1eb4008127c5b15976f9ff5a7913020f12818
aaguuscaan <aaguuscaan@gmail.com>
2026-06-09 20:41:57 -0300

    Configurar Firebase con variables de entorno de Vercel

---
+++ api/firebase-config.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+  });
+}
